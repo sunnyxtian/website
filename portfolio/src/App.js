949f11aaa195ca71964c06b1a3d96d7868015236
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-09-18 17:58:25 -0700

    reactifying my website

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Intro from './components/Intro';
+
+function App () {
+  return (
+    <div>
+      <Intro />
+    </div>
+  )
+}
+
+export default App;
---
+++ portfolio/src/components/Intro.js
@@ -0,0 +1,10 @@
+function Intro() {
+  return (
+    <div>
+      <h1>Sunny Tian</h1>
+      <p>uw informatics junior studying hci.</p>
+    </div>
+  )
+}
+
+export default Intro;
