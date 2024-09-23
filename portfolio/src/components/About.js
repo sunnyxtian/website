2ef7d68831d80b2cde89d5b2ac40d91cf566112b
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-09-23 14:47:56 -0700

    added favicon

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -11,24 +11,6 @@ function About () {
           communication.
         </p>
       </div>
-      <h3 className="text-lg font-semibold">Find me at</h3>
-      <div className="text-sm flex">
-        <div>
-          <p><a href="mailto:sunnytia@uw.edu">Sunnytia@uw.edu</a></p>
-        </div>
-        <div>
-          <p><a href="https://www.linkedin.com/in/sunnyxtian/" target="_blank">LinkedIn</a></p>
-        </div>
-        <div>
-          <p><a href="https://www.github.com/sunnyxtian/" target="_blank">Github</a></p>
-        </div>
-        <div>
-          <p>
-            No rights reserved. <a href="https://creativecommons.org/publicdomain/zero/1.0/"
-            target="_blank">CCO 1.0</a>
-          </p>
-        </div>
-      </div>
     </section>
   )
 }
--- portfolio/src/components/Sidebar.js
+++ portfolio/src/components/Sidebar.js
@@ -1,8 +1,28 @@
 function Sidebar() {
   return (
     <section className="sidebar">
-      <h1 className="text-2xl font-semibold">SUNNY TIAN</h1>
-      <p className="font-semibold">designer and developer</p>
+      <div>
+        <h1 className="text-2xl font-semibold">SUNNY TIAN</h1>
+        <p className="font-semibold">designer and developer</p>
+      </div>
+
+      <div className="text-sm flex">
+        <div>
+          <p><a href="mailto:sunnytia@uw.edu">Sunnytia@uw.edu</a></p>
+        </div>
+        <div>
+          <p><a href="https://www.linkedin.com/in/sunnyxtian/" target="_blank">LinkedIn</a></p>
+        </div>
+        <div>
+          <p><a href="https://www.github.com/sunnyxtian/" target="_blank">Github</a></p>
+        </div>
+        <div>
+          <p>
+            No rights reserved. <a href="https://creativecommons.org/publicdomain/zero/1.0/"
+            target="_blank">CCO 1.0</a>
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
