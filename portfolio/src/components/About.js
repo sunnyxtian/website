a17c9010fa19362fad185a5cfc5a84b3fdf27b12
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-09-24 01:11:41 -0700

    adding hover states & color on hover

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -7,8 +7,15 @@ function About () {
           I'm a junior in Informatics at the University of Washington.
 
           I'm interested in the ways coding and design intersect, from creating
-          generative designs in Processing to improving developer and designer
+          generative designs in Processing to improving developer to designer
           communication.
+        </p>
+      </div>
+
+      <div className="text-sm">
+        <p>
+          No copyright. <a href="https://creativecommons.org/publicdomain/zero/1.0/"
+          target="_blank">CCO 1.0 &#8599;</a>
         </p>
       </div>
     </section>
--- portfolio/src/components/Sidebar.js
+++ portfolio/src/components/Sidebar.js
@@ -1,26 +1,20 @@
 function Sidebar() {
   return (
-    <section className="sidebar">
+    <section className="sidebar flex flex-row">
       <div>
-        <h1 className="text-2xl font-semibold">SUNNY TIAN</h1>
+        <h1>SUNNY TIAN</h1>
         <p className="font-semibold">designer and developer</p>
       </div>
 
-      <div className="text-sm flex">
+      <div className="links text-right">
         <div>
-          <p><a href="mailto:sunnytia@uw.edu">Sunnytia@uw.edu</a></p>
+          <p><a href="mailto:sunnytia@uw.edu">Sunnytia@uw.edu &#8599;</a></p>
         </div>
         <div>
-          <p><a href="https://www.linkedin.com/in/sunnyxtian/" target="_blank">LinkedIn</a></p>
+          <p><a href="https://www.linkedin.com/in/sunnyxtian/" target="_blank">LinkedIn &#8599;</a></p>
         </div>
         <div>
-          <p><a href="https://www.github.com/sunnyxtian/" target="_blank">Github</a></p>
-        </div>
-        <div>
-          <p>
-            No rights reserved. <a href="https://creativecommons.org/publicdomain/zero/1.0/"
-            target="_blank">CCO 1.0</a>
-          </p>
+          <p><a href="https://www.github.com/sunnyxtian/" target="_blank">Github &#8599;</a></p>
         </div>
       </div>
     </section>
