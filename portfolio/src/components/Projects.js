cef4cbce2c293f52cae2f57716a7c9990f6c31c7
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2024-09-24 03:42:22 -0700

    adding images to projects

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -4,19 +4,26 @@ function Projects() {
       <section className="projects">
         <h2 className="">Projects</h2>
         <div class="grid grid-cols-3 gap-4">
-          <article>
-            <h3 className="text-lg font-semibold">Study Timer</h3>
-            <h4 className="">Project management</h4>
-            <p className="text-sm">June 2024 - present</p>
+          <article className="project-card">
+            <img src="imgs/timer.png" alt="screenshot of pomodoro timer interface"></img>
+            <h3 className="font-semibold">Study Timer</h3>
+            <div className="tags text-sm">
+              <p>Project Management</p>
+              <p>UI/UX</p>
+              <p>Front-End Development</p>
+            </div>
           </article>
-          <article>
-            <h3 className="text-lg font-semibold">Husky UXplorers</h3>
-            <h4 className="">Web development</h4>
-            <p className="text-sm">June 2024 - present</p>
+          <article className="project-card">
+            <img src="imgs/husky_uxplorers.png" alt="screenshot of purple themed website">
+            </img>
+            <h3 className="font-semibold">Husky UXplorers</h3>
+            <div className="tags text-sm">
+              <p>Front-End Development</p>
+            </div>
           </article>
         </div>
       </section>
-      
+
       <hr></hr>
     </div>
   )
