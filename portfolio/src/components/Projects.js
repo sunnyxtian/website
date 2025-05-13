e35cf763649d5cd65149a9a421737045d8ffa13b
sunnyxtian <134821187+sunnyxtian@users.noreply.github.com>
2025-05-12 22:14:58 -0700

    added mono years for projects

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,15 +1,26 @@
 function Projects() {
+  function handleHover() {
+    // First, format project bios as a JSON.
+    // Then, add an image path within the JSON.
+    // Populate the list of projects dynamically
+      // Can create seperate, projectCard component that takes in the JSON data.
+    // This function will take the key or some identifying value
+      // then finds the corresponding image to display
+  }
+
   return(
     <div>
       <section className="projects flex flex-col">
-        <div className="mb-5">
+        <div className="project-spreads mb-5">
           <h2>Muse Magazine Spreads</h2>
           <p className="blurb">Publication Design</p>
+          <p className="tags text-base text-gray font-mono">2025</p>
         </div>
 
-        <div className="mb-5">
+        <div className="project-social mb-5">
           <h2>Muse Social Media</h2>
           <p className="blurb">Social Media Design</p>
+          <p className="tags text-base text-gray font-mono">2025</p>
         </div>
       </section>
     </div>
--- portfolio/src/routes/App.js
+++ portfolio/src/routes/App.js
@@ -7,14 +7,27 @@ import Footer from '../components/Footer';
 
 function App () {
   return (
-    <div className="flex flex-col lg:flex-row items-center">
-      <div className="w-2/3 lg:w-1/3 lg:mr-20 mb-20 lg:mb-0">
-        <Header />
-        <Footer />
-      </div>
-      <div className="w-2/3 lg:w-1/3">
-        <Projects />
-      </div>
+    <div>
+      <section className="content flex flex-col lg:flex-row items-center z-10">
+        <div className="w-2/3 lg:w-1/5 lg:mr-20 mb-20 lg:mb-0">
+          <Header />
+          <Footer />
+        </div>
+
+        <div className="w-2/3 lg:w-1/3">
+          <Projects />
+        </div>
+
+        <div className="project-images w-1/3 z-0">
+          <div className="project-spreads">
+            <img src="imgs/when-you-say-yes.png" alt="two page magazine spread"></img>
+          </div>
+
+          <div className="project-social">
+            <img src="imgs/vinyl-revival.png" alt="social media post with text about otherworld wine bar"></img>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
